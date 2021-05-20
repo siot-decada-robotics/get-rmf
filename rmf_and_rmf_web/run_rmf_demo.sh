a2ba16497df311af499b89855a40f71d67935a43
pingjuh <45278408+pingjuh@users.noreply.github.com>
2021-05-20 15:18:23 +0800

    Add files via upload

---
+++ rmf_and_rmf_web/run_rmf_demo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source ~/rmf_ws/install/setup.bash
+ros2 launch rmf_demos office.launch.xml headless:=true
---
+++ rmf_and_rmf_web/run_rmf_web.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source ~/rmf_ws/install/setup.bash
+npm start --prefix ~/rmf_ws/rmf-web/packages/dashboard
