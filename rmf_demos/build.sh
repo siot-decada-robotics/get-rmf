f90a38536ff0f5881d715b50eb0f78c3d9bec9fc
Scottie C <48780312+scott-cjx@users.noreply.github.com>
2021-05-29 18:45:29 +0800

    Update build.sh

--- rmf_demos/build.sh
+++ rmf_demos/build.sh
@@ -2,11 +2,7 @@
 
 echo " -> build_rmfdemos"
 function build_rmfdemos {
-	cd ws-rmf_demos && \
-	{
-		source /opt/ros/noetic/setup.bash | source /opt/ros/noetic/setup.zsh
-	} && \
-	colcon build --cmake-args -DCMAKE_BUILD_TYPE=RELEASE && \
-	cd ..
+	source /opt/ros/noetic/setup.bash | source /opt/ros/noetic/setup.zsh && \
+	colcon build --cmake-args -DCMAKE_BUILD_TYPE=RELEASE
 }
 
